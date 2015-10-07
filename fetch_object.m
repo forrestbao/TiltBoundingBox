27061290c59b75d10802d6d47957df03faa167e5
Yichuan Zhao <yichuan.g.zhao@gmail.com>
2015-10-07 16:37:37 -0400

    change method of reproducing coordinates

--- fetch_object.m
+++ fetch_object.m
@@ -1,36 +1,22 @@
-function [mask im] = fetch_object(image, angle, ux, uy, w, h, zoom)
-image_size = 400*(1+zoom/10);
-image = imresize(image, [image_size image_size]);
-xc = 158;
-yc = 183;
+function [mask im] = fetch(image, angle, Ax, Ay, w, h)
 
-im = imrotate(image,360-angle);
-imSize = size(im,1);
+im = image;
+imWidth = size(image,2);
+imHeight = size(image,1);
 
-s = imSize/2;
+Bx = round(Ax + w*cos(angle));
+By = round(Ay - w*sin(angle));
 
-xs = xc + image_size/2;
-ys = yc + image_size/2;
+Dx = round(Ax + h*sin(angle));
+Dy = round(Ay + h*cos(angle));
 
-xe = xs - s;
-ye = ys - s;
+Cx = Bx + (Dx - Ax);
+Cy = By + (Dy - Ay);
 
-xt = round(ux - xe);
-yt = round(uy - ye);
+x0 = [Ax Bx; Bx Cx; Cx Dx; Dx Ax];
+y0 = [Ay By; By Cy; Cy Dy; Dy Ay];
 
-x1 = xt + w;
-y1 = yt;
-
-x2 = xt + w;
-y2 = yt + h;
-
-x3 = xt;
-y3 = yt + h;
-
-x0 = [xt x1; x1 x2; x2 x3; x3 xt];
-y0 = [yt y1; y1 y2; y2 y3; y3 yt];
-
-mask = zeros(imSize,imSize);
+mask = zeros(imHeight,imWidth);
 mask = getMask(mask, x0, y0, 1);
 mask = imfill(mask);
 mask = uint8(mask);
@@ -39,6 +25,3 @@ for i=1:3
     im(:,:,i) = im(:,:,i).*mask;
 end
 imshow(im);
-end
-
-
